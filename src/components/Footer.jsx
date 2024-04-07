94a9d04b5928d4d8effa56cd74352d0253b65b91
Smit <smitmpatil123@gmail.com>
2024-04-07 19:30:02 +0530

    Footer Devlopment

---
+++ src/components/Footer.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const Footer = () => {
+    return (
+
+        <>
+
+            <footer className='w-full h-32 border border-white'>
+
+                
+
+            </footer>
+
+        </>
+
+    )
+}
+
+export default Footer
