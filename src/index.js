420701a8e9e350dddca76eb43536f07a3f191925
Smit <smitmpatil123@gmail.com>
2024-04-07 19:28:00 +0530

    Router Setup

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter as Router } from 'react-router-dom';
 
 import './index.css';
 import App from './App';
@@ -9,7 +10,9 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <ContractProvider>
         <React.StrictMode>
-            <App />
+            <Router>
+                <App />
+            </Router>
         </React.StrictMode>
     </ContractProvider>
 );
